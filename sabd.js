3215c8945eb767fe12c2d33b2e52fef2a507bac2
Reimlex <86831223+Reimlex@users.noreply.github.com>
2022-07-05 21:25:46 +0300

    Create sabd.js

---
+++ sabd.js
@@ -0,0 +1,32 @@
+// Script by Reimlex
+// GitHub: https://github.com/Reimlex
+
+function isBlocking() {
+    var blocking = false;
+    var element = document.getElementById('sabd');
+    if (element) {
+        let CL = ['ad', 'ads', 'adsbox', 'doubleclick', 'ad-placement', 'ad-placeholder', 'adbadge', 'BannerAd', 'banner', 'adsbygoogle'];
+        for (let item of CL) {
+            element.classList.add(item);
+        }
+        if (window.getComputedStyle(element).getPropertyValue('display') == 'none') {
+            blocking = true;
+        }
+    } else {
+        blocking = false;
+    }
+    return blocking;
+}
+
+function isBlockingGoogleAds() {
+    var blocking = false;
+    try {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js', false);
+        xhr.send();
+        blocking = xhr.status != 200;
+    } catch (ex) {
+        blocking = true;
+    }
+    return blocking;
+}
